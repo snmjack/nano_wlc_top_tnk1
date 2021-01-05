ae43fb6d25816af6ff4d2d5e0893aacf00ece0c6
Swapnil Mahajan <31958717+snmjack@users.noreply.github.com>
2021-01-05 23:16:45 +0530

    Add files via upload

---
+++ nano_wlc_rx_box_bottom_json_test_v6.ino
@@ -0,0 +1,222 @@
+//************************************************************
+// Water Level Controller V 1.0
+//
+// Master LCD Program serial receiver
+//************************************************************
+
+// Necessary Libraries
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Necessary for OLED Display
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Define Relay Pin
+# define relayPin 3
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+const size_t capacity = JSON_OBJECT_SIZE(3) + 75;
+DynamicJsonDocument doc(capacity);
+
+const unsigned char Motoricon [] PROGMEM = {
+  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x80, 0x7f, 0xff, 
+  0xff, 0x80, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xe0, 0x00, 0x01, 0xff, 0xc0, 0x00, 0x00, 0x7f, 
+  0xcc, 0xff, 0xce, 0x7f, 0xcc, 0xff, 0xcf, 0x3f, 0xcc, 0x00, 0x0f, 0x3f, 0xcc, 0x00, 0x0f, 0x3f, 
+  0xcc, 0xff, 0xcf, 0x3f, 0xcc, 0xff, 0xcf, 0x3f, 0xcc, 0x00, 0x0f, 0x03, 0xcc, 0x00, 0x0f, 0x03, 
+  0xcc, 0xff, 0xcf, 0x3f, 0xcc, 0xff, 0xcf, 0x3f, 0xcc, 0x00, 0x0f, 0x3f, 0xcc, 0x00, 0x0f, 0x3f, 
+  0xcc, 0xff, 0xcf, 0x3f, 0xcc, 0xff, 0xce, 0x7f, 0xc0, 0x00, 0x00, 0x7f, 0xe0, 0x00, 0x01, 0xff, 
+  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf8, 0x00, 0x07, 0xff, 0xf8, 0x00, 0x07, 0xff, 
+  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+};
+
+int topsensor_status = 1;
+int bottomsensor_status = 1;
+int overheadlevel = 0;
+int tankEmpty = 90;
+
+bool relay_status = false;
+int relay_state = LOW;
+char* motor_status;
+
+unsigned long previousTimeOled = millis();
+long timeIntervalOled = 1;
+
+unsigned long previousTimeRec = millis();
+long timeIntervalRec = 1;
+
+unsigned long previousTimeMotor = millis();
+long timeIntervalMotor = 1;
+
+unsigned long previousTimeManual = millis();
+long timeIntervalManual = 1;
+
+
+// Setup
+void setup() {
+  // Begin Serial communication at a baudrate of 9600:
+  Serial.begin(4800);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);// initialize with the I2C addr 0x3D (for the 128x64)
+  //getJsonData();
+  //display.setTextColor(WHITE);
+  //display.setTextSize(2);
+  //display.setCursor(0, 0);
+  //display.print("Setup Mode");
+  display.display();
+  delay(5000);
+  display.clearDisplay(); 
+
+ }
+
+
+ void loop() {
+  
+   unsigned long currentTime = millis();
+
+   if (Serial.available() > 0) {
+             //getJsonData();
+          
+
+   
+   // Task 1 - Get Jason Data
+      if(currentTime - previousTimeRec > timeIntervalRec) {
+         previousTimeRec = currentTime;
+          if (Serial.available() > 0) {
+             getJsonData();
+          }
+      }
+      
+   // Task 2 - Control Motor as per sendor status
+      if(currentTime - previousTimeMotor > timeIntervalMotor) {
+         previousTimeMotor = currentTime;
+         //motorFunction();
+      }
+   
+   
+   // Task 3 - Draw Tank and display level
+      if(currentTime - previousTimeOled > timeIntervalOled) {
+         previousTimeOled = currentTime;
+         display.clearDisplay();
+         motorFunction();
+         motorDisplayFunction();
+         //display.display();
+         displayOhtLevel(SCREEN_HEIGHT, overheadlevel);
+         display.display();
+         
+      }
+      
+   
+  }
+    else {
+             display.setTextColor(WHITE);
+             display.setTextSize(2);
+             display.setCursor(0, 30);
+             display.print("No Data Received");
+             display.display();
+      //delay(1000);
+             //display.clearDisplay();
+         }
+  
+  }
+
+
+  void getJsonData() {
+  deserializeJson(doc, Serial);
+  topsensor_status = doc["topsensor_status"];
+  bottomsensor_status = doc["bottomsensor_status"];
+  overheadlevel = doc["overheadlevel"];
+  /* display.setTextColor(WHITE);
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.print("Top - ");
+  display.setCursor(90, 0);
+  display.print(topsensor_status);
+  display.setCursor(0, 20);
+  display.print("Botom- ");
+  display.setCursor(100, 20);
+  display.print(bottomsensor_status);
+  display.setCursor(0, 40);
+  display.print("Tank - ");
+  display.setCursor(90, 40);
+  display.print(overheadlevel);
+  display.display(); */
+}
+
+void displayOhtLevel(int height, int ohtLevel) {
+  int final_height = 64 - ((height*ohtLevel)/tankEmpty);
+  if(final_height <= 10) {
+    final_height = 10;
+  }
+  if(final_height > 64) {
+    final_height = 64;
+  }
+  //int final_height_rev = 64 - final_height;
+  display.setTextSize(1);
+  display.setCursor(71,0);
+  display.print("Over Head");
+  display.drawRect(69, 10, 59, 54, WHITE);
+  display.fillRect(69, final_height, 59, 54, WHITE);
+  //display.display();
+}
+
+void motorDisplayFunction() {
+  /*//motor_status = "";
+  if(relay_status  == true) {
+    motor_status = "ON";
+  } 
+  if(relay_status  == false) {
+    motor_status = "OFF";
+  }*/
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  
+  /*display.setCursor(34,10);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.print(motor_status);
+  //display.display();*/
+}
+
+void motorFunction() {
+  
+  display.setCursor(34,10);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  
+  if(topsensor_status == 1 && bottomsensor_status == 1){
+    //Both level low
+    motor_status = "OFF";
+    display.print("OFF");
+   // display.display();
+    //relay_status = false;
+    relay_state = LOW;
+  } 
+  
+  if(topsensor_status == 0 && bottomsensor_status == 0){
+      //Bottom level high & top level high
+      motor_status = "ON";
+      display.print("ON");
+     // display.display();
+      //relay_status = true;
+      relay_state = HIGH;
+      }
+   
+   if (topsensor_status == 0 && bottomsensor_status == 1) {
+      display.println(motor_status);
+    }
+   
+   if (topsensor_status == 1 && bottomsensor_status == 0) {
+      display.print(motor_status);
+    }
+  
+ 
+  
+  digitalWrite(relayPin, relay_state);
+  display.drawBitmap(0, 0,  Motoricon, 32, 32, WHITE);
+  
+}
